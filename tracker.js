7a472fce663a93c623de40aa7eeff07b0586f749
JGdmd <JGdmd@proton.me>
2023-03-13 20:31:38 +0100

    async / await ipAddress and domain name

--- tracker.js
+++ tracker.js
@@ -11,43 +11,53 @@ let header = document.querySelector('header');
 let infoAddress = document.querySelector('.info-address');
 let cross = document.getElementById('cross');
 let tokenPublic = "pk.eyJ1IjoiZGV2ZXhwbG9yaXMiLCJhIjoiY2xmNWllbjltMWNtazNybGp0OWd5MWpoaiJ9.EB5SRwYcg5M_gbfHF5eVPA";
-function SearchIp(url) {
-    if (url !== apiOnLoad) {
+
+async function FetchApi(url) {
+    if(url !== apiOnLoad) {
         url = 'https://geo.ipify.org/api/v2/country,city?apiKey=at_krAgkVfXkGjs1FhIXlhUjKCk3qexX&ipAddress=' + inputIp.value;
     }
-    fetch(url).then(response => {
-        console.log(response);
+    let api = await fetch(url).then((response) => {
         if (response.ok) {
             return response.json();
         } else {
-            fetch('https://geo.ipify.org/api/v2/country,city?apiKey=at_krAgkVfXkGjs1FhIXlhUjKCk3qexX&domain=' + inputIp.value).then(response => {
-                if (response.ok) {
-                    return response.json();
-                } throw new Error('Try Again');
-            })
+            throw new Error('IP Address unknown');
         }
-    })
-        .then(data => {
-            ip.innerText = data.ip;
-            locationIp.innerText = data.location.country + ' - ' + data.location.region + ' - ' + data.location.city;
-            timezone.innerText = 'UTC ' + data.location.timezone;
-            isp.innerText = data.isp;
-            let geocoding = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+ data.location.city +".json?access_token=" + tokenPublic;
-            fetch(geocoding)
-                .then(responses => responses.json())
-                .then(city => {
-                    let array = {'longitude' : city.features[0].center[0]};
-                    array.latitude = city.features[0].center[1];
-                    let map = L.map('map', {
-                        zoomControl: false
-                    }).setView([array.latitude, array.longitude],10);
-                    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                        maxZoom: 19,
-                        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                    }).addTo(map);
-                    L.marker([array.latitude, array.longitude]).addTo(map);
-                    L.circle([array.latitude, array.longitude], {radius: 10000}).addTo(map);
-                });
+    }).catch(error => console.log(error));
+    if (api === undefined) {
+        api = await fetch('https://geo.ipify.org/api/v2/country,city?apiKey=at_krAgkVfXkGjs1FhIXlhUjKCk3qexX&domain=' + inputIp.value).then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Domain unknown');
+            }
+        }).catch(error => console.log(error));
+        if(api === undefined) {
+            return null;
+        }
+    }
+    return api;
+}
+async function SearchIp(url = '') {
+    let data = await FetchApi(url);
+    ip.innerText = data.ip;
+    locationIp.innerText = data.location.country + ' - ' + data.location.region + ' - ' + data.location.city;
+    timezone.innerText = 'UTC ' + data.location.timezone;
+    isp.innerText = data.isp;
+    let geocoding = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+ data.location.city +".json?access_token=" + tokenPublic;
+    fetch(geocoding)
+        .then(responses => responses.json())
+        .then(city => {
+            let array = {'longitude' : city.features[0].center[0]};
+            array.latitude = city.features[0].center[1];
+            let map = L.map('map', {
+                zoomControl: false
+            }).setView([array.latitude, array.longitude],10);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+            L.marker([array.latitude, array.longitude]).addTo(map);
+            L.circle([array.latitude, array.longitude], {radius: 10000}).addTo(map);
         });
 }
 
@@ -61,7 +71,7 @@ button.addEventListener('click', () => {
         divMap.id = 'map';
         header.after(divMap);
     }
-    SearchIp(apiAsk)
+    SearchIp()
 })
 form.addEventListener('submit',(e) => {
     if(map != null) {
@@ -71,7 +81,7 @@ form.addEventListener('submit',(e) => {
         header.after(divMap);
     }
     e.preventDefault();
-    SearchIp(apiAsk);
+    SearchIp();
 })
 
 cross.addEventListener('click', () => {
